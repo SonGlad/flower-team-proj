37e40be70c24ca3f1a5ab8683232aa872d4751b6
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-03-13 23:14:32 +0200

    jj

---
+++ src/js/slick.js
@@ -0,0 +1,29 @@
+$('.responsive').slick({
+  // dots: true,
+  prevArrow: '<button type="button" class="my-slick-prev">&xlarr;</button>',
+  nextArrow: '<button type="button" class="my-slick-next">&xrarr;</button>',
+  infinite: true,
+  speed: 300,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  slidesToShow: 3,
+  variableWidth: true, // обовязково додається, щоб частина картки заходило за вьюпорт
+
+  responsive: [
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
