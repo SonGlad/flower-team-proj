77fe5f808963fed36476bdc36d387de827e8c4e4
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-03-13 21:48:25 +0200

    +styles on slick
    partial

---
+++ src/js/slick.js
@@ -0,0 +1,11 @@
+$('.responsive').slick({
+  // dots: true,
+  prevArrow: '<button type="button" class="my-slick-prev">&xlarr;</button>',
+  nextArrow: '<button type="button" class="my-slick-next">&xrarr;</button>',
+  infinite: false,
+  speed: 300,
+  slidesToScroll: 1,
+//   adaptiveHeight: true,
+  slidesToShow: 3,
+  
+});
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import "./js/modals"
 import "./js/mobile"
 import "./js/modal-blog"
+import "./js/slick"
