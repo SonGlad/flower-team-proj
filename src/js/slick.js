5f588ae4dc4500bfa6fe3f06d80e5d56fa39f1c5
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-03-14 03:12:57 +0200

    created arrows in slider

--- src/js/slick.js
+++ src/js/slick.js
@@ -4,24 +4,24 @@ $('.autoplay').slick({
   autoplay: true,
   autoplaySpeed: 2000,
   // // dots: true,
-  // prevArrow: '<button type="button" class="my-slick-prev">&xlarr;</button>',
-  // nextArrow: '<button type="button" class="my-slick-next">&xrarr;</button>', // обовязково додається, щоб частина картки заходило за вьюпорт
+  prevArrow: '<button type="button" class="my-swipe-prev">&xlarr;</button>',
+  nextArrow: '<button type="button" class="my-swipe-next">&xrarr;</button>', // обовязково додається, щоб частина картки заходило за вьюпорт
 
-  autoplay: [
-    {
-      breakpoint: 1280,
-      settings: {
-        slidesToShow: 3,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-      },
-    },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ],
+  // autoplay: [
+  //   {
+  //     breakpoint: 1280,
+  //     settings: {
+  //       slidesToShow: 3,
+  //     },
+  //   },
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       slidesToShow: 2,
+  //     },
+  //   },
+  //   // You can unslick at a given breakpoint now by adding:
+  //   // settings: "unslick"
+  //   // instead of a settings object
+  // ],
 });
