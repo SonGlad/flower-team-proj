c53b08dbdc5050a026ba5cbcb37ed9e5271e238d
SonGlad <okoshevy@gmail.com>
2023-03-16 03:32:01 +0200

    Correction

--- src/js/header-scroll.js
+++ src/js/header-scroll.js
@@ -1,5 +1,5 @@
 window.onscroll = function () {
-    if (window.screen.width >= 768) {
+    if (window.screen.width >= 1280) {
       scrollY <= 815
         ? document.querySelector('.header-js').classList.remove('header-bg')
         : document.querySelector('.header-js').classList.add('header-bg');
