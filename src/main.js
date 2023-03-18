7f7eb37c9414d9fdc3ff1de588546c20f37971f6
SonGlad <okoshevy@gmail.com>
2023-03-18 02:24:23 +0200

    Pre-last modify JS, header/footer css

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import "./js/slick"
 import "./js/mobile"
 import "./js/form-validation"
-import "./js/scroll-to-top"
-import "./js/team-modal"
+import "./js/header-&-scroll-to-top"
 import "./js/modals"
-import "./js/modal-blog"
-import "./js/header-scroll"
 import "./js/store.js"
 
 
